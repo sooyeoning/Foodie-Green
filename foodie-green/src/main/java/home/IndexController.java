eaa83350fd6c52233606c07bd4a8291f87b6b4b0
DoHyunDaniel <132706505+DoHyunDaniel@users.noreply.github.com>
2023-08-23 09:01:31 +0900

    Update IndexController.java

--- foodie-green/src/main/java/home/IndexController.java
+++ foodie-green/src/main/java/home/IndexController.java
@@ -2,20 +2,30 @@ package home;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
-	HomeService homeservice;
+	HomeService homeService;
 	
 
 	@RequestMapping("/")
 	public String index() {
-		return "index"; 
+		return "/index"; 
 	}
+	
+	@RequestMapping("/vision")
+	public String vision() {
+		return "/home/vision"; 
+	}
+	
+	@RequestMapping("/faq")
+	public String faq() {
+		return "/home/faq"; 
+	}
+
 
 	
 }
